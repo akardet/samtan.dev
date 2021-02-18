d0957aa98c8de880cd8cd052c2faba8bd06cef9f
Sam Tancharoensuksavai <stancharoen@gmail.com>
2021-02-17 22:26:49 -0600

    add mobile nav bar

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,7 +1,31 @@
+import { useEffect, useRef } from "react";
 import Link from "next/link";
 import styles from "./Navigation.module.scss";
+import { throttle } from "../../utils/throttle";
 
 const Navigation = ({}) => {
+  const navContainerRef = useRef(null);
+  const pageYOffsetRef = useRef(0);
+
+  const showNavigation = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    if (pageYOffsetRef.current > currentScrollPos) {
+      navContainerRef.current.style.bottom = "0";
+    } else {
+      navContainerRef.current.style.bottom = "-100px";
+    }
+    pageYOffsetRef.current = currentScrollPos;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", throttle(showNavigation));
+
+    return () => {
+      window.removeEventListener("scroll", throttle(showNavigation));
+    };
+  }, []);
+
   return (
     <nav className={styles["nav"]}>
       <Link href="/">
@@ -13,7 +37,7 @@ const Navigation = ({}) => {
           />
         </div>
       </Link>
-      <ul className={styles["container"]} tabIndex={0}>
+      <ul ref={navContainerRef} className={styles["container"]} tabIndex={0}>
         <Link href="/about">
           <li className={styles["item"]}>
             <a tabIndex={0}>About</a>
