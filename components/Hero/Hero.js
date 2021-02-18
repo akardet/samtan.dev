2c4ca6d9c10ed13087cafca184bd2af7500dc101
Sam Tancharoensuksavai <stancharoen@gmail.com>
2021-02-17 22:26:09 -0600

    fix mobile Beans subtitle

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -15,20 +15,24 @@ const Hero = ({}) => {
           className={styles["hero__profile-arrow"]}
           src={"/images/arrow.png"}
         />
-        <div className={styles["hero__profile-beans"]}>
+        <div className={styles["hero__subtext"]}>
           <p>
-            <span className={cx(styles["h4"], styles["bold"])}>Mr. Beans</span>
+            <span
+              className={cx(styles["hero__subtext-heading"], styles["bold"])}
+            >
+              Mr. Beans
+            </span>
             <br />
             CTO, Chief Treat Officer
           </p>
         </div>
       </div>
-      <article className={styles["hero__text"]}>
+      <article className={styles["hero__textbox"]}>
         <h1 className={styles["hero__title"]}>
           Hi <span className={styles["wave"]}>👋🏼</span> I’m Sam
           Tancharoensuksavai
         </h1>
-        <p>
+        <p className={styles["hero__text"]}>
           I am a <span className={styles["bold"]}>Frontend Engineer</span>{" "}
           working on building elegant mobile web apps at{" "}
           <Link href={"/work/#apple"}>
@@ -36,7 +40,7 @@ const Hero = ({}) => {
           </Link>
           .
         </p>
-        <p>
+        <p className={styles["hero__text"]}>
           In my 5+ years as a developer, I’ve had my hands on nearly all parts
           of a modern software stack, but I’ve found my passion lies at the
           intersection where design and software meet.{" "}
