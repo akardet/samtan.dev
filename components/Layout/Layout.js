9255e4f6bea04d8294fc972f5fbf881e8fa1d4cb
Sam Tancharoensuksavai <stancharoen@gmail.com>
2021-03-20 21:11:35 -0500

    Update share metadata

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -13,14 +13,10 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Hi, I'm Sam Tancharoensuksavai. I'm in a constant pursuit to find a way to blend my passion for
+          technology and my love of design"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content={`/images/banner2.jpg`} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
         <link
