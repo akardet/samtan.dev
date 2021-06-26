3fd77d25ffdf7d33badbe22a43ae9eb21234301c
Sam Tancharoensuksavai <stancharoen@gmail.com>
2021-06-25 22:29:21 -0500

    add twitter share banner

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -8,14 +8,17 @@ export default function Layout({ children, home }) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Hi, I'm Sam Tancharoensuksavai. I'm in a constant pursuit to find a way to blend my passion for
-          technology and my love of design"
-        />
-        <meta property="og:image" content={`/images/banner2.jpg`} />
-        <meta name="og:title" content={siteTitle} />
+
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@samtan_" />
+        <meta name="twitter:title" content="Sam Tancharoensuksavai" />
+        <meta
+          name="twitter:description"
+          content="In a constant pursuit to find a way to blend my passion for technology and my love of design"
+        />
+        <meta name="twitter:image" content="/images/banner.jpg" />
+        <meta property="og:image" content={`/images/banner.jpg`} />
+        <meta name="og:title" content="Sam Tancharoensuksavai" />
         <link
           rel="preload"
           href="/fonts/Poppins/Poppins-Regular.ttf"
