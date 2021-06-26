170473649341bb63b6e92ca00e7a39f7f5d87cbf
Sam Tancharoensuksavai <stancharoen@gmail.com>
2021-06-25 22:34:04 -0500

    fix twitter share card

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -9,16 +9,22 @@ export default function Layout({ children, home }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
 
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="@samtan_" />
-        <meta name="twitter:title" content="Sam Tancharoensuksavai" />
+        <meta
+          name="twitter:title"
+          content="Sam Tancharoensuksavai | Frontend Engineer"
+        />
         <meta
           name="twitter:description"
-          content="In a constant pursuit to find a way to blend my passion for technology and my love of design"
+          content="I'm in a constant pursuit to find a way to blend my passion for technology and my love of design"
         />
         <meta name="twitter:image" content="/images/banner.jpg" />
         <meta property="og:image" content={`/images/banner.jpg`} />
-        <meta name="og:title" content="Sam Tancharoensuksavai" />
+        <meta
+          name="og:title"
+          content="Sam Tancharoensuksavai | Frontend Engineer"
+        />
         <link
           rel="preload"
           href="/fonts/Poppins/Poppins-Regular.ttf"
