e824615041c8d9c28ad3e21f89fdf9d92db1edb6
Sam Tancharoensuksavai <stancharoen@gmail.com>
2021-06-25 22:55:38 -0500

    fix twitter card image

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children, home }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
 
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content="@samtan_" />
         <meta
           name="twitter:title"
@@ -19,7 +19,10 @@ export default function Layout({ children, home }) {
           name="twitter:description"
           content="I'm in a constant pursuit to find a way to blend my passion for technology and my love of design"
         />
-        <meta name="twitter:image" content="/images/banner.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://res.cloudinary.com/stancharoen/image/upload/v1624679690/banner_x1wahd.jpg"
+        />
         <meta property="og:image" content={`/images/banner.jpg`} />
         <meta
           name="og:title"
