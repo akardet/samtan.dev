9e28938444f6f6ba51fb9cad0eed7db2c4754d60
Sam Tancharoensuksavai <stancharoen@gmail.com>
2024-08-30 22:55:11 -0700

    initial astro commit

--- components/Layout/index.js
+++
@@ -1,4 +0,0 @@
-import Layout from "./Layout";
-
-export const siteTitle = "Sam Tancharoensuksavai, Destroyer of DOMs";
-export default Layout;
--- next.config.js
+++
@@ -1,5 +0,0 @@
-const withPlugins = require("next-compose-plugins");
-const withSvgr = require("next-svgr");
-const withTM = require("next-transpile-modules")(["gsap", "gsap/Draggable"]);
-
-module.exports = withPlugins([withSvgr, withTM]);
