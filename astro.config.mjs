35b7a9fbe6234a4b4a302115a2f71f5eb577573c
Sam Tancharoensuksavai <stancharoen@gmail.com>
2024-08-30 23:21:41 -0700

    remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  adapter: vercel()
-});
+export default defineConfig({});
